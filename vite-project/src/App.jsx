9a92ff69eae82a18cfc82a5cdea6ba58e480bd4a
tsoni2 <tsoni2@ncsu.edu>
2025-06-24 12:22:01 -0700

    Introduced state management using useState; demonstrated real-time reactivity with onClick and onChange handlers; explored per-component state isolation and dynamic rendering

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,18 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from './assets/Button'
+import { Button } from './Button'
+import { Textbox } from './Textbox'
 
 function App() {
-  function notify(msg){
-    alert(msg)
-  }
 
   return (
     <>
-      <Button text = {"Sample Text"} color = {"Red"} func = {notify} msg = {"Bttn 1 clicked"}/>
-      <Button text = {"Sandbox"} color = {"Blue"} func = {notify} msg = {"Bttn 2 hits different!"}/>
-      <Button text = {"Testing"} color = {"Green"}/>
+      <Textbox/>
     </>
   )
 }
---
+++ vite-project/src/Button.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export function Button({color}){
+
+    const [count, setCount] = useState(0)
+    
+    return(
+        <button onClick={() => setCount(count + 1)} style = {{backgroundColor: color}}>
+            <p>{count}</p>
+        </button>
+    )
+}
