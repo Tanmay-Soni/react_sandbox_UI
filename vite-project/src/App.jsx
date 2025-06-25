288437a0a2ddf16da5720ada7550e2da9c22105a
tsoni2 <tsoni2@ncsu.edu>
2025-06-24 22:28:40 -0700

    Rendered lists using .map(), passed props to list items, and displayed object-based lists

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { Button } from './Button'
 import { Textbox } from './Textbox'
+import { TextBlock } from './TextBlock'
+import { StoreIterm } from './StoreItem'
+import './App.css'
 
 function App() {
+  const item1 = {title: "Bat", desc: "Cricket Bat", price: 50}
+  const item2 = {title: "Ball", desc: "Cricket Ball", price: 10}
+  const item3 = {title: "Wicket", desc: "Cricket Wicket", price: 20}
+
+  const myArray = [item1, item2, item3]
 
   return (
     <>
-      <Textbox/>
+      {myArray.map((element, index) => {
+        return(
+          <StoreIterm item = {element} key = {index}/>
+        )
+      })}
     </>
   )
 }
---
+++ vite-project/src/StoreItem.jsx
@@ -0,0 +1,10 @@
+export function StoreIterm({item}){
+
+    return(
+        <>
+            <h1>{item.title}</h1>
+            <p>{item.desc}</p>
+            <button>{item.price}</button>
+        </>
+    )
+}
